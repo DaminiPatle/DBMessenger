62f4ae7b58f90c096c55b1bd9a68d2cd697df333
Bhushan Lilhare <bhushanlilhare956@gmail.com>
2023-12-24 13:17:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dbmessenger/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.dbmessenger;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() == null) {
+            Intent intent = new Intent(MainActivity.this, login.class);
+            startActivity(intent);
+        }
+    }
+}
